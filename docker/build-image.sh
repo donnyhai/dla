eb02a906a7e13ca171f74160a08c56ed6d8140de
donnykong <donnykong@pop-os.localdomain>
2021-01-26 15:59:08 +0100

    deleted all

--- docker/build-image.sh
+++
@@ -1,1 +0,0 @@
-docker build -t dla:latest Dockerfile --network=host
--- docker/start-container.sh
+++
@@ -1,3 +0,0 @@
-mountdir="/home/donnykong/Schreibtisch/dla/docker/mount-dir/simulation"
-
-docker run -it -v $mountdir:/app -w /app dla:latest bash
